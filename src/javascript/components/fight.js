f541258de63192ff453fd8e1a178f690a3066563
esquiafo <leocapone@hotmail.com>
2023-12-12 05:26:55 -0300

    + feat(fight): damage and status bar

--- src/javascript/components/fight.js
+++ src/javascript/components/fight.js
@@ -1,56 +1,6 @@
 import controls from '../../constants/controls';
 import showWinnerModal from './modal/winner';
 
-export async function fight(firstFighter, secondFighter) {
-    const p1 = { ...firstFighter };
-    const p2 = { ...secondFighter };
-    p1.player = 'P1';
-    p2.player = 'P2';
-
-    const maxHealthP1 = firstFighter.health;
-    const maxHealthP2 = secondFighter.health;
-
-    return new Promise(resolve => {
-        const pressedKeys = {};
-
-        document.addEventListener('keypress', event => {
-            if (firstFighter.health <= 0 || secondFighter.health <= 0) {
-                showWinnerModal(firstFighter.health <= 0 ? secondFighter : firstFighter);
-                resolve();
-            } else {
-                document.getElementById('left-fighter-indicator').style.width = `${
-                    (firstFighter.health * 100) / maxHealthP1
-                }%`;
-                document.getElementById('right-fighter-indicator').style.width = `${
-                    (secondFighter.health * 100) / maxHealthP2
-                }%`;
-                pressedKeys[event.code] = true;
-                Object.keys(pressedKeys).forEach(() => {
-                    if (!pressedKeys[controls.PlayerOneBlock] && !pressedKeys[controls.PlayerTwoBlock]) {
-                        if (pressedKeys[controls.PlayerOneAttack]) {
-                            this.getDamage(firstFighter, secondFighter);
-                        }
-                        if (pressedKeys[controls.PlayerTwoAttack]) {
-                            this.getDamage(secondFighter, firstFighter);
-                        }
-                    }
-                });
-            }
-        });
-
-        document.addEventListener('keyup', event => {
-            delete pressedKeys[event.code];
-        });
-    });
-}
-
-export function getDamage(attacker, defender) {
-    const damage = Math.max(0, parseInt(this.getHitPower(attacker), 10) - parseInt(this.getBlockPower(defender), 10));
-    const newHealth = defender.health - damage;
-    const updatedDefender = { ...defender, health: newHealth };
-    return updatedDefender;
-}
-
 export function getHitPower(fighter) {
     const randomNumber = Math.floor(Math.random() * 2) + 1;
     const power = randomNumber * fighter.attack;
@@ -63,7 +13,59 @@ export function getBlockPower(fighter) {
     return power;
 }
 
+export function getDamage(attacker, defender) {
+    const defenderCopy = { ...defender };
+    const damage = Math.max(1, parseInt(getHitPower(attacker), 10) - parseInt(getBlockPower(defenderCopy), 10));
+    defenderCopy.health = Math.max(0, defenderCopy.health - damage);
+    Object.assign(defender, { health: defenderCopy.health });
+    return damage;
+}
+
 export function specialAttack(fighter) {
     const power = fighter.attack * 2;
     return power;
+}
+
+export async function fight(firstFighter, secondFighter) {
+    const playerOne = { ...firstFighter };
+    const playerTwo = { ...secondFighter };
+    playerOne.player = 'Player 1';
+    playerTwo.player = 'Player 2';
+    const maxHealthplayerOne = firstFighter.health;
+    const maxHealthplayerTwo = secondFighter.health;
+
+    return new Promise(resolve => {
+        const pressedKeys = {};
+
+        document.addEventListener('keypress', event => {
+            if (playerOne.health <= 0 || playerTwo.health <= 0) {
+                playerOne.health <= 0 ? showWinnerModal(playerTwo) : showWinnerModal(playerOne);
+                resolve(null);
+            } else {
+                document.getElementById('left-fighter-indicator').style.width = `${
+                    (playerOne.health * 100) / maxHealthplayerOne
+                }%`;
+                document.getElementById('right-fighter-indicator').style.width = `${
+                    (playerTwo.health * 100) / maxHealthplayerTwo
+                }%`;
+                pressedKeys[event.code] = true;
+                // eslint-enable no-unused-expressions
+                Object.keys(pressedKeys).forEach(() => {
+                    if (!pressedKeys[controls.PlayerOneBlock] && !pressedKeys[controls.PlayerTwoBlock]) {
+                        if (pressedKeys[controls.PlayerOneAttack]) {
+                            getDamage(playerOne, playerTwo);
+                        }
+                        if (pressedKeys[controls.PlayerTwoAttack]) {
+                            getDamage(playerTwo, playerOne);
+                        }
+                    }
+                });
+                // eslint-enable no-unused-expressions
+            }
+        });
+
+        document.addEventListener('keyup', event => {
+            delete pressedKeys[event.code];
+        });
+    });
 }
